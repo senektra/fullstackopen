b085e07c5338dc59d3b24051978fbc47ee1e3576
senektra <rcvallada@gmail.com>
2024-03-02 20:52:05 -0800

    Point notes service to backend url

--- part3/lessons/notes-front/src/services/notes.js
+++ part3/lessons/notes-front/src/services/notes.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/api/notes'
+const baseUrl = 'https://fso-notes-backend.up.railway.app/api/notes'
 
 const getAll = () => {
   return axios.get(baseUrl).then(res => res.data)
