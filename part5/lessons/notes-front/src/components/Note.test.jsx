b74d8125b1b0a4bcfb907f4b5a27808ee22737a5
senektra <rcvallada@gmail.com>
2024-04-25 19:06:27 -0700

    Add testing to note component button

--- part5/lessons/notes-front/src/components/Note.test.jsx
+++ part5/lessons/notes-front/src/components/Note.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Note from './Note'
 
 test('renders content', () => {
@@ -8,7 +9,26 @@ test('renders content', () => {
   }
 
   render(<Note note={note} />)
-
   const element = screen.getByText('Component testing is done with react-testing-library')
+
   expect(element).toBeDefined()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const note = {
+    content: 'Component testing is done with react-testing-library',
+    important: true
+  }
+
+  const mockHandler = vi.fn()
+
+  render(
+    <Note note={note} toggleImportant={mockHandler} />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('make not important')
+  await user.click(button)
+
+  expect(mockHandler.mock.calls).toHaveLength(1)
 })
