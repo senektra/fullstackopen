70a417f585848f7491283137b90ccffab32d4548
senektra <rcvallada@gmail.com>
2024-05-01 20:28:01 -0700

    Add dev dependencies for testing

--- part5/exercises/blog-front/.eslintrc.cjs
+++ part5/exercises/blog-front/.eslintrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: { 
+    browser: true, 
+    es2020: true, 
+    'vitest-globals/env': true, 
+  },
   globals: {
     '__API_URL__': false
   },
